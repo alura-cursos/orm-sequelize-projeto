4183d37715c89e1dc1f14bfbd4490df860d38456
JulianaAmoasei <late.parrot@gmail.com>
2020-05-21 12:32:34 -0300

    video 3.3

--- api/index.js
+++ api/index.js
@@ -1,17 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
-
-app.use(bodyParser.json())
-
 const port = 3000
 
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({ 
-    mensagem: 'boas-vindas à API' 
-}))
+routes(app)
 
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
