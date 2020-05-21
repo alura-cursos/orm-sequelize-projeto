34ae0baa9cbf600540735afe583ba2f421e4c0d3
JulianaAmoasei <late.parrot@gmail.com>
2020-05-21 13:35:54 -0300

    video 3.5

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,20 @@ class PessoaController {
       return res.status(500).json(error.message)
     }
   }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params
+    try {
+      const umaPessoa = await database.Pessoas.findOne( { 
+        where: { 
+          id: Number(id) 
+        }
+      })
+      return res.status(200).json(umaPessoa)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
 }
 
 module.exports = PessoaController
