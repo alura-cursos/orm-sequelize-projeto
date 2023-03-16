f5b7fc1f768f4c3d04f8c05725314977cbec07b1
Emerson Laranja <emersonlaranja@gmail.com>
2023-03-16 09:57:36 -0300

    Update seed-turmas.js

--- arquivos_base/seed-turmas.js
+++ arquivos_base/seed-turmas.js
@@ -3,29 +3,29 @@ module.exports = {
 		return queryInterface.bulkInsert('Turmas', [
 			{
 				data_inicio: "2020-02-01",
-				nivel: 1,
-				docente: 6,
+				nivel_id: 1,
+				docente_id: 6,
 				createdAt: new Date(),
 				updatedAt: new Date()				 
 			},
 			{
 				data_inicio: "2020-02-01",
-				nivel: 2,
-				docente: 5,
+				nivel_id: 2,
+				docente_id: 5,
 				createdAt: new Date(),
 				updatedAt: new Date()			
 			},
 			{
 				data_inicio: "2020-02-01",
-				nivel: 3,
-				docente: 6,
+				nivel_id: 3,
+				docente_id: 6,
 				createdAt: new Date(),
 				updatedAt: new Date()			
 				},
 			{
 				data_inicio: "2020-07-01",
-				nivel: 3,
-				docente: 6,
+				nivel_id: 3,
+				docente_id: 6,
 				createdAt: new Date(),
 				updatedAt: new Date()			
 			}
